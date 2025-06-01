e4c09a3ff51a44168b17ac41c2c612c540bda258
Vitalii Balabanov <tertiumnon@gmail.com>
2025-06-01 21:39:42 +0200

    repo type fix

--- types/repo/repo.types.ts
+++ types/repo/repo.types.ts
@@ -7,9 +7,9 @@ import {
 } from "../api/api-response.types";
 
 export interface Repo<T, CreateDto, UpdateDto> {
-  findAll(): Promise<ApiResponseFindAll<T, unknown>>;
-  findOne(id: string): Promise<ApiResponseFindOne<T, unknown>>;
-  create(dto: CreateDto): Promise<ApiResponseCreate<T, unknown>>;
-  update(id: string, dto: UpdateDto): Promise<ApiResponseUpdate<T, unknown>>;
-  delete(id: string): ApiResponseDelete<void>;
+  findAll(): Promise<ApiResponseFindAll<T>>;
+  findOne(id: string): Promise<ApiResponseFindOne<T>>;
+  create(dto: CreateDto): Promise<ApiResponseCreate<T>>;
+  update(id: string, dto: UpdateDto): Promise<ApiResponseUpdate<T>>;
+  delete(id: string): Promise<ApiResponseDelete>;
 }
